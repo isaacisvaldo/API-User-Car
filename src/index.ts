4f911f205fbe7789ec7faf5399e9dcb07b01f0a7
Isaac Isvaldo <91834847+IsaacIsvaldo@users.noreply.github.com>
2023-04-07 17:44:38 +0100

    mmm

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-import express from "express";
+import express from 'express';
+import { config } from "dotenv";
+
+config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port,()=>console.log(`Rodadndo na Porta ${port}`));
