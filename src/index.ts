45091b99808a9002ab81e1a2de41cece48a046b5
Isaac Isvaldo <91834847+IsaacIsvaldo@users.noreply.github.com>
2023-04-11 15:14:24 +0100

    jjj

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import express from 'express';
 import { config } from "dotenv";
 import { routes } from './router';
+import {MongoClient} from './databases/mongo'
 
 config();
-const app = express();
-app.use(express.json());
-app.use(routes)
-const port = process.env.PORT || 3000;
 
-
+const  main = async ()=>{
+    const app = express();
+    app.use(express.json());
+    app.use(routes)
+    const port = process.env.PORT || 3000;
+    
+await MongoClient.connect()
 app.listen(port,()=>console.log(`Rodadndo na Porta ${port}`));
+}
+
+main();
